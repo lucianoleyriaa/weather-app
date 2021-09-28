6d4bdba55b7fac366d6ea122d0b31dbae2f42af5
lucianoleyriaa <lucholeyria@gmail.com>
2021-09-28 18:30:59 -0300

    Implementacion de axios para realizar peticion al api del clima

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
 import "./App.css";
 import Form from "./components/Form";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
 function App() {
+   const [data, setData] = useState(null);
+   const [weather, setWeather] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const [apiError, setApiError] = useState(null);
+
+   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+   useEffect(() => {
+      if (!data) return;
+      fetchWeather("https://api.openweathermap.org/data/2.5/weather", data);
+      setLoading(true);
+      setWeather(null);
+      setApiError(false);
+   }, [data]);
+
+   const fetchWeather = async (url, { city, country }) => {
+      try {
+         const params = {
+            q: `${city}, ${country}`,
+            appid: `${API_KEY}`,
+            units: "metric",
+         };
+
+         const response = await axios(url, { params });
+
+         setLoading(false);
+         setWeather(response.data);
+      } catch (e) {
+         setApiError("No se encuentra la ubicacion ingresada");
+         setLoading(false);
+      }
+   };
+
    return (
-      <div className="bg-secondary">
+      <div className="bg-primary" style={{ minHeight: "25rem" }}>
          <div className="container pt-5 ">
-            <div className="col-md-6">
-               <Form />
+            <div className="row">
+               <div className="col-md-6">
+                  <Form sendRequest={setData} />
+               </div>
+               <div className="col-md-6 text-center">
+                  {loading && (
+                     <div class="spinner-border text-warning" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                     </div>
+                  )}
+                  {weather && <Card weatherInfo={weather} />}
+                  {apiError && (
+                     <div className="alert alert-danger">{apiError}</div>
+                  )}
+               </div>
             </div>
-            <div className="col-md-6"></div>
          </div>
       </div>
    );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,67 @@
 import React from "react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
+   const [data, setData] = useState({ city: "", country: "" });
+   const [error, setError] = useState(false);
+
+   const onChangeHandler = (e) => {
+      setData((prevState) => {
+         return { ...prevState, [e.target.name]: e.target.value };
+      });
+   };
+
+   const { city, country } = data;
+
+   const onSubmitHandler = (e) => {
+      e.preventDefault();
+
+      if (city === "" || country === "") return setError(true);
+
+      setError(false);
+
+      props.sendRequest(data);
+   };
+
    return (
       <Fragment>
-         <form>
+         <form onSubmit={onSubmitHandler}>
+            {error && (
+               <div className="alert alert-danger">
+                  Todos los campos son obligatorios
+               </div>
+            )}
             <div className="form-group">
                <label className="text-white mb-2">City</label>
-               <input type="text" className="form-control" />
+               <input
+                  type="text"
+                  name="city"
+                  className="form-control"
+                  onChange={onChangeHandler}
+                  value={city}
+                  placeholder="Enter a city"
+               />
             </div>
             <div className="form-group mt-3">
                <label className="text-white mb-2">Country</label>
-               <select className="form-select">
-                  <option value="argentina">Argentina</option>
-                  <option value="chile">Chile</option>
-                  <option value="brasil">Brasil</option>
-                  <option value="uruguay">Uruguay</option>
-                  <option value="paraguay">Paraguay</option>
+               <select
+                  className="form-select"
+                  name="country"
+                  onChange={onChangeHandler}
+                  value={country}
+               >
+                  <option value="">--Seleccionar--</option>
+                  <option value="AR">Argentina</option>
+                  <option value="CL">Chile</option>
+                  <option value="BR">Brasil</option>
+                  <option value="UY">Uruguay</option>
+                  <option value="PY">Paraguay</option>
+                  <option value="BO">Bolivia</option>
                </select>
             </div>
-            <button className="btn btn-success mt-3 mb-3">Buscar</button>
+            <div className="d-grid mt-3">
+               <button className="btn btn-warning mt-3 mb-3 ">Buscar</button>
+            </div>
          </form>
       </Fragment>
    );
